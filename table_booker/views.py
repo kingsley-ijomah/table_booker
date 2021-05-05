291f9b7e6177496367e59119bbedddcc469076d4
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-05-05 18:26:50 +0100

    Initial directory setup

--- table_booker/views.py
+++ table_booker/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
+
+def home_page(request):
+    return render(request, "table_booker/home.html", context={})
+
+
+def login_page(request):
+    return render(request, "table_booker/login.html", context={})
+
+
+def signup_page(request):
+    return render(request, "table_booker/signup.html", context={})
+
+
+def logout_page(request):
+    return render(request, "table_booker/logout.html", context={})
