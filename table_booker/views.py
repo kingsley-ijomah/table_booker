f9a7eca7732990ecdf034ebfac8666dde138e11d
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-05-05 18:48:46 +0100

    Adding home page with authentication

--- table_booker/views.py
+++ table_booker/views.py
@@ -4,18 +4,24 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 
+from .models import Restaurant
+
 
 def home_page(request):
-    return render(request, "table_booker/home.html", context={})
+    if not request.user.is_authenticated:
+        return redirect("table_booker:login")
+
+    context = {"restaurants": Restaurant.objects.all()}
+    return render(request, "home.html", context=context)
 
 
 def login_page(request):
-    return render(request, "table_booker/login.html", context={})
+    return render(request, "login.html", context={})
 
 
 def signup_page(request):
-    return render(request, "table_booker/signup.html", context={})
+    return render(request, "signup.html", context={})
 
 
 def logout_page(request):
-    return render(request, "table_booker/logout.html", context={})
+    return render(request, "logout.html", context={})
