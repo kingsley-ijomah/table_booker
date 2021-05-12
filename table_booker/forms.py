75f40d5c3b3d7377812d0e7a05092cc19d92cf08
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-05-12 19:23:54 +0100

    adding validation

--- table_booker/forms.py
+++ table_booker/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 from .models import Booking
 
@@ -33,3 +35,10 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ("table", "date")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        date = cleaned_data["date"]
+
+        if date < timezone.now():
+            raise ValidationError("Date cannot be in the past")
